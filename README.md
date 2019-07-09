64c4df518d02c25d2185a974dcccc6ad0936c0ca
maximilionus <maksimka-up@yandex.ru>
2019-07-09 23:55:03 +0300

    Almost done with all planned functional. /set_idle_array not finished

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 ### MAXIMILI's Steam Hour Booster for Deploys
 | Var                | Sample       | Description                                                                                                            |
 | ------------------ | ------------ | ---------------------------------------------------------------------------------------------------------------------- |
-| STEAM_LOGIN        | strloginname | Steam login [str]                                                                                                      |
-| STEAM_PASSWORD     | strpasswd    | Steam passwd [str]                                                                                                     |
-| STEAM_2FA          | 2FA Code     | Steam 2FA code [str]                                                                                                   |
-| STEAM_GAMEIDS      | [XXX,XXX]    | Steam items for idle [int arr]                                                                                         |
-| STEAM_FORCEIDLE    | true         | Force steam to kick user with different ip from game to start idle process [bool]                                      |
-| CORE_SHUFFLE_DELAY | 1000         | Steam idle restart with STEAM_GAMEIDS shuffle procedure delay in ms [int]                                              |
-| TBOT_ENABLE        | true         | Enable/Disable Telegram control bot [bool]                                                                             |
-| TBOT_TOKEN         | tokenstr     | Telegram bot token if enabled [str]                                                                                    |
-| TBOT_ACCESSID      | 83737333983  | Telegram bot will compare userID with this int after `/start` command and ignore unauthorized users (if enabled) [int] |
+| STEAM_LOGIN        | strloginname | Steam login `[str]`                                                                                                      |
+| STEAM_PASSWORD     | strpasswd    | Steam passwd `[str]`                                                                                                     |
+| STEAM_2FA          | 2FA Code     | Steam 2FA code `[str]`                                                                                                   |
+| STEAM_GAMEIDS      | [XXX,XXX]    | Steam items for idle `[int arr]`                                                                                         |
+| STEAM_FORCEIDLE    | true         | Force steam to kick user with different ip from game to start idle process `[bool]`                                      |
+| CORE_SHUFFLE_DELAY | 1000         | Steam idle restart with STEAM_GAMEIDS shuffle procedure delay in ms `[int]`                                              |
+| TBOT_ENABLE        | true         | Enable/Disable Telegram control bot `[bool]`                                                                             |
+| TBOT_TOKEN         | tokenstr     | Telegram bot token if enabled `[str]`                                                                                    |
+| TBOT_ACCESSID      | 83737333983  | Telegram bot will compare userID with this int after `/start` command and ignore unauthorized users (if enabled) `[int]` |
+
+### Telegram bot commands-list
+```
+current_idle_array - Return array with idling IDs
+set_idle_array - Override current idle array with new values
+time - Get all collected time data
+```
--- mxhboost.js
+++ mxhboost.js
@@ -3,14 +3,26 @@ const Telegraf = require('telegraf')
 
 const forceIdle = JSON.parse(process.env.STEAM_FORCEIDLE)
 const idleList_shuffle_ms = JSON.parse(process.env.CORE_SHUFFLE_DELAY)
+
+//Init timers
+var data_collected = {
+	timeFromStartup: 0,
+	timeFromShuffle: 0
+}
+setInterval(function () {
+	data_collected.timeFromStartup++
+	data_collected.timeFromShuffle++
+}, 1000)
+
 //Activate interval for idleList shuffle
-setInterval(function() {
-	idleList = idleList.sort(function(){return .5 - Math.random();})
+setInterval(function () {
+	idleList = idleList.sort(function () { return .5 - Math.random(); })
 	client.gamesPlayed(idleList, forceIdle);
+	data_collected.timeFromShuffle = 0;
 	console.log(`Idle array successfully shuffled and restarted idle process for GameID${Array.isArray(idleList) && idleList.length > 1 ? 's' : ''} [${idleList}]`)
 }, idleList_shuffle_ms)
 
-var idleList = JSON.parse(process.env.STEAM_GAMEIDS.split(",")).sort(function(){return .5 - Math.random();})
+var idleList = JSON.parse(process.env.STEAM_GAMEIDS.split(",")).sort(function () { return .5 - Math.random(); })
 
 if (idleList.length < 1) {
 	console.log('No games selected')
@@ -38,20 +50,38 @@ client.on('loggedOn', details => {
 // Some error occurred during logon
 client.on('error', e => console.log(e));
 
-client.logOn(logOnDetails)
+//client.logOn(logOnDetails) //TODO:Use in final build
+
 
 //Init telegram bot
 if (JSON.parse(process.env.TBOT_ENABLE)) {
 	const tg_bot = new Telegraf(process.env.TBOT_TOKEN)
 
+	function forceChangeIdleArr(ctx){
+		inputString = ctx.message.text.replace('/set_idle_array', '')
+
+		idleList = JSON.parse((inputString).split(","))
+		idleList = idleList.sort(function () { return .5 - Math.random(); })
+		client.gamesPlayed(idleList, forceIdle);
+		data_collected.timeFromShuffle = 0;
+
+		ctx.reply(`Idle array was successfully force overridden with\n${ctx.message.text.replace('/set_idle_array ', '')}\n\nFinal variant of array:\n${idleList}`)
+		console.log(`TBOT: Idle array was force overridden by user\nNew array: [${idleList}]`)
+	}
+
 	function checkTGUser(userId) {
 		if (userId == JSON.parse(process.env.TBOT_ACCESSID)) { //User id check
-			console.log(`TGBOT: Authorized user '${userId}' is online`)
-
-			tg_bot.telegram.sendMessage(userId, 'You are connected to MXSteamHourBooster control system. Welcome user!');
-			tg_bot.command('current_idle_array', (ctx) => ctx.reply(`Current idle list ${process.env.STEAM_GAMEIDS}`))
+			console.log(`TBOT: Authorized user '${userId}' is online`)
+			tg_bot.telegram.sendMessage(userId, 'You are connected to MXSteamHourBooster control system. Welcome!');
+			tg_bot.command('current_idle_array', (ctx) => ctx.reply(process.env.STEAM_GAMEIDS))
+			tg_bot.command('time', (ctx) => ctx.reply(`
+			Time from script run (h/m/s): ${Math.floor(data_collected.timeFromStartup / 3600)}:${Math.floor(data_collected.timeFromStartup % 3600 / 60)}:${data_collected.timeFromStartup % 3600 % 60}\nTime from last idle array shuffle (h/m/s): ${Math.floor(data_collected.timeFromShuffle / 3600)}:${Math.floor(data_collected.timeFromShuffle % 3600 / 60)}:${Math.floor(data_collected.timeFromShuffle % 3600 % 60)}
+			`))
+			tg_bot.command('set_idle_array', (ctx) => forceChangeIdleArr(ctx))
 		} else {
-			console.log(`TGBOT: Access for user[${userId}] was denied.`)
+			console.log(`TBOT: Access for user[${userId}] was denied.`)
+			tg_bot.on('message', (ctx) => console.log(`TBOT: Received message from unauthorized user\n- ${ctx.message.from.username}[id:'${ctx.message.from.id}']: ${ctx.message.text}`))
+			//Send all messages from unauthorized users to log
 		}
 	}
 
